11b51ea3b8a33dfd3cb4b5ea97f0b5d3c154e5af
funkunux <funkunho@qq.com>
2018-08-04 20:57:29 +0800

    build the framework of demos

---
+++ source/include/cppDemo.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <iostream>
+
+#define debug(format, args...) printf("[%s:%u] " format, __FUNCTION__, __LINE__, ##args)
+#define DEMO_SUCC 0
+#define DEMO_FAIL 1
+
+using namespace std;
