96ebc52aaa970b497786bb5c1c58274eab13a6f0
funkunux <funkunho@qq.com>
2018-08-04 20:57:29 +0800

    build the framework of demos

---
+++ build.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+ShowUsage()
+{
+    echo "Usage: $0 [OPT]"
+    echo "  OPT: -r   Rebuild all demos!"
+    echo "       -c   Clean all demos!"
+    echo "       -h   Show this usage!"
+}
+
+Clean()
+{
+    echo "Clean All"
+    [ -d "./build" ] && rm -rf build
+    rm -f demo_*
+}
+
+Build()
+{
+    echo "Building..."
+    [ ! -d "./build" ] && mkdir build
+    cd build
+    cmake ..
+    [ -f Makefile ] && make && echo "Done!"
+    ls -l ../demo_*
+}
+
+if [ $# -gt 1 ]; then
+    ShowUsage
+    exit 0
+fi
+
+if [ $# -eq 1 ]; then
+    case $1 in
+        -r)
+            Clean
+            Build
+            ;;
+        -c)
+            Clean
+            ;;
+        -h)
+            ShowUsage
+            ;;
+        *)
+            ShowUsage
+            ;;
+    esac
+    exit 0
+fi
+
+Build
---
+++ source/STL_Demos/demo_vector.cpp
@@ -0,0 +1,7 @@
+#include "cppDemo.h"
+
+int main()
+{
+    debug("Hello~\n");
+    return DEMO_SUCC;
+}
